050910f3af00b624d69fb589b9bffc72722207cf
lubnaurooj <lubnaurooj.017@gmail.com>
2018-05-19 14:35:12 +0800

    First Commit for problem1 selecting Maven Project template

---
+++ Problem1/com.amazon/src/main/java/visaChandler/com/amazon/App.java
@@ -0,0 +1,13 @@
+package visaChandler.com.amazon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
